530128856307aa2deab2f84e935770293e4a265e
yashrampersad <yash.rampersad.2007@gmail.com>
2025-12-01 12:08:07 +0000

    test

---
+++ Day 1/day1.py
@@ -0,0 +1,74 @@
+instructions = []
+
+with open("Day 1/input.txt") as file:
+    for line in file:
+        instructions.append([line[0], int(line[1:])])
+
+def part1():
+    total = 0
+    current = 50
+    for instruction in instructions:
+        if instruction[0] == "L":
+            current -= instruction[1]
+        else:
+            current += instruction[1]
+        current = current % 100
+        if current == 0:
+            total += 1
+
+    print(total)
+
+def part2():
+    # total = 0
+    # current = 50
+    # for instruction in instructions:
+    #     prev = current
+    #     if instruction[0] == "L":
+    #         current -= instruction[1]
+    #     else:
+    #         current += instruction[1]
+    #     print(prev, current)
+    #     if prev * current < 0:
+    #         # print("click")
+    #         # print ("adding ", abs(current // 100))
+    #         total += abs(current // 100)
+    #     elif current == 0 or current == 100:
+    #         # print("click")
+    #         total += 1
+    #     elif abs(current) > 100:
+    #         # print("click")
+    #         # print ("adding ", abs(current // 100))
+    #         total += abs(current // 100)
+    #     current = current % 100
+    # print(total)
+
+    total = 0
+    current = 50
+    for instruction in instructions:
+        prev = current
+        if instruction[0] == "L":
+            current -= instruction[1]
+        else:
+            current += instruction[1]
+        print(prev, instruction[0], current)
+        if current < 0 and current % 100 == 0:
+            print("adding ", abs(current // 100)+1)
+            total += abs(current // 100) + 1
+        elif (prev==0 and current <=-100):
+            print("adding ", abs(current // 100)-1)
+            total += abs(current // 100)-1
+        elif (current < 0 and prev != 0) or current >= 100:
+            print("adding ", abs(current // 100))
+            total += abs(current // 100)
+        if current == 0:
+            print("adding 1")
+            total += 1
+        current = current % 100
+
+    print(total)
+
+print(-101//100)
+# 6547 too low
+# 5941 too low
+
+# 6554
